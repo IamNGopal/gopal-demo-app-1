d9ecfb778a671f83997e6b4d72e50e6b84c7ae6d
Gopal <nanigopal@zkteco.in>
2021-12-02 10:37:05 +0530

    added

---
+++ src/main/java/com/example/demo/Controller2.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller2 {
+
+	@GetMapping("/")
+	public String helloGet(@RequestParam String name) {
+		return name;
+	}
+
+	@PostMapping("/")
+	public String helloPost(@RequestBody TestDTO testDTO) {
+		return testDTO.toString();
+	}
+
+}
---
+++ src/main/java/com/example/demo/TestDTO.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class TestDTO {
+	private String name;
+}
